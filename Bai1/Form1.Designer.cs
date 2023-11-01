063b56684256ef76ecc3e632b692bff0de3d1c3c
Luu Thuy Minh Nhat <minhnhat240820032019@gmail.com>
2023-11-01 14:17:52 +0700

    add Bai1 and Demo

---
+++ Bai1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMaKhoa = new System.Windows.Forms.Label();
+            this.lbTenKhoa = new System.Windows.Forms.Label();
+            this.txtMaKhoa = new System.Windows.Forms.TextBox();
+            this.txtTenKhoa = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbMaKhoa
+            // 
+            this.lbMaKhoa.AutoSize = true;
+            this.lbMaKhoa.Location = new System.Drawing.Point(26, 66);
+            this.lbMaKhoa.Name = "lbMaKhoa";
+            this.lbMaKhoa.Size = new System.Drawing.Size(50, 13);
+            this.lbMaKhoa.TabIndex = 0;
+            this.lbMaKhoa.Text = "Ma Khoa";
+            this.lbMaKhoa.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbTenKhoa
+            // 
+            this.lbTenKhoa.AutoSize = true;
+            this.lbTenKhoa.Location = new System.Drawing.Point(26, 111);
+            this.lbTenKhoa.Name = "lbTenKhoa";
+            this.lbTenKhoa.Size = new System.Drawing.Size(54, 13);
+            this.lbTenKhoa.TabIndex = 1;
+            this.lbTenKhoa.Text = "Ten Khoa";
+            this.lbTenKhoa.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // txtMaKhoa
+            // 
+            this.txtMaKhoa.Location = new System.Drawing.Point(77, 66);
+            this.txtMaKhoa.Name = "txtMaKhoa";
+            this.txtMaKhoa.Size = new System.Drawing.Size(100, 20);
+            this.txtMaKhoa.TabIndex = 2;
+            this.txtMaKhoa.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtTenKhoa
+            // 
+            this.txtTenKhoa.Location = new System.Drawing.Point(77, 111);
+            this.txtTenKhoa.Name = "txtTenKhoa";
+            this.txtTenKhoa.Size = new System.Drawing.Size(100, 20);
+            this.txtTenKhoa.TabIndex = 3;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(12, 169);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 4;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(102, 169);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(187, 169);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 6;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtTenKhoa);
+            this.Controls.Add(this.txtMaKhoa);
+            this.Controls.Add(this.lbTenKhoa);
+            this.Controls.Add(this.lbMaKhoa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbMaKhoa;
+        private System.Windows.Forms.Label lbTenKhoa;
+        private System.Windows.Forms.TextBox txtMaKhoa;
+        private System.Windows.Forms.TextBox txtTenKhoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+    }
+}
+
---
+++ Bai1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Bai1
+{
+    public partial class Form1 : Form
+    {
+        string strketnoi = @"Data Source=A209PC02;Initial Catalog=QL_KHOA; Integrated Security=True";
+        SqlConnection sqlketnoi = null;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                if (sqlketnoi == null)
+                {
+                    sqlketnoi = new SqlConnection(strketnoi);
+                }
+                if (sqlketnoi.State == ConnectionState.Closed)
+                {
+                    sqlketnoi.Open();
+                    MessageBox.Show("Ket noi thanh cong");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string add = "insert into dbo.Khoa values ('" + txtMaKhoa.Text + "','" + txtTenKhoa.Text+"')";
+            SqlCommand cmd = new SqlCommand(add, sqlketnoi);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Them thanh cong!");
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
